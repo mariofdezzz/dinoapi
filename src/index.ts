42b658d37e91d5faa7428b06e1c9380cf9f93c81
Mario Ferrero <47794126+mariofdezzz@users.noreply.github.com>
2023-01-13 20:04:36 +0000

    feat: log grapql url at startup

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { serve } from 'http';
 import { router } from '/router/index.ts';
 
-serve(router);
+serve(router, {
+	onListen({ port }) {
+		console.log(`Listening on http://localhost:${port}/graphql`);
+	},
+});
