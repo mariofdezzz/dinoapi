286c7cb3ac79bf0c82ed4de5e012502b524cd171
Mario Ferrero <47794126+mariofdezzz@users.noreply.github.com>
2023-01-23 17:06:37 +0000

    feat: improve Query gql
    Using new db controllers

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,11 +1,58 @@
+import {
+	DBDino,
+	DBEpoch,
+	DBFamily,
+	DBPeriod,
+	DBSpecie,
+} from '/db/models/types.ts';
 import { DB } from '/db/index.ts';
 
 const db = new DB();
 
 export const resolvers = {
 	Query: {
-		allDinos: async () => {
-			return await db.getAllDino();
+		async dinos() {
+			return await db.dinos.findAll();
+		},
+		async periods() {
+			return await db.periods.findAll();
+		},
+		async period(_: unknown, { id }: { id: number }) {
+			return await db.periods.find(id);
+		},
+		async era(_: unknown, { id }: { id: number }) {
+			return await db.eras.find(id);
+		},
+		async epochs() {
+			return await db.epochs.findAll();
+		},
+	},
+	Dino: {
+		async temporalRange(dino: DBDino) {
+			return await db.epochs.find(dino.temporalRange);
+		},
+		async specie(dino: DBDino) {
+			return await db.specie.find(dino.specie);
+		},
+	},
+	Specie: {
+		async family(specie: DBSpecie) {
+			return await db.family.find(specie.family);
+		},
+	},
+	Family: {
+		async kingdom(family: DBFamily) {
+			return await db.kingdom.find(family.kingdom);
+		},
+	},
+	Epoch: {
+		async period(epoch: DBEpoch) {
+			return await db.periods.find(epoch.period);
+		},
+	},
+	Period: {
+		async era(period: DBPeriod) {
+			return await db.eras.find(period.era);
 		},
 	},
 };
